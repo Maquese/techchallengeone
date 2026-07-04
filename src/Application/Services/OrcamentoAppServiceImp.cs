fcb1301e601b30e40ad39df73589dec9fe931493
Maquese <kenneymaquese@gmail.com>
2026-07-04 11:40:18 -0300

    melhora de nomenclatura aplication para application

---
+++ src/Application/Services/OrcamentoAppServiceImp.cs
@@ -0,0 +1,20 @@
+﻿using Aplication.Models;
+using Domain.Aggregates;
+using Domain.Exceptions;
+using Aplication.Interfaces;
+
+namespace Aplication.Services;
+
+public class OrcamentoAppServiceImp
+{
+    private readonly OrcamentoRepository _orcamentoRepository;
+    private readonly OrdemServicoRepository _ordemServicoRepository;
+
+    public OrcamentoAppServiceImp(OrcamentoRepository orcamentoRepository, OrdemServicoRepository ordemServicoRepository)
+    {
+        _orcamentoRepository = orcamentoRepository;
+        _ordemServicoRepository = ordemServicoRepository;
+    }
+
+
+}
