3d7e7cd03d717073fdb2c594b4272db06b53a610
Maquese <kenneymaquese@gmail.com>
2026-07-14 20:03:45 -0300

    adicionando o components para rodar corretamente o hpa

--- infra/main.tf
+++ infra/main.tf
@@ -20,11 +20,12 @@ resource "null_resource" "apply_k8s_manifests" {
       filemd5(abspath("${path.module}/../k8s/deployment.yaml")),
       filemd5(abspath("${path.module}/../k8s/service.yaml")),
       filemd5(abspath("${path.module}/../k8s/metrics.yaml")),
+      filemd5(abspath("${path.module}/../k8s/components.yaml")),
       filemd5(abspath("${path.module}/../k8s/hpa.yaml")),
     ])
   }
 
   provisioner "local-exec" {
-    command = "kubectl apply -f \"${abspath("${path.module}/../k8s/volume.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/persistClaim.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/deployment-sql.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/service-sql.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/secret.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/environment.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/deployment.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/service.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/metrics.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/hpa.yaml")}\""
+    command = "kubectl apply -f \"${abspath("${path.module}/../k8s/volume.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/persistClaim.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/deployment-sql.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/service-sql.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/secret.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/environment.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/deployment.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/service.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/metrics.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/components.yaml")}\" && kubectl apply -f \"${abspath("${path.module}/../k8s/hpa.yaml")}\""
   }
 }
